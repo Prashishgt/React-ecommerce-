67745b3150495bd6f0ee7b797cdfa13e0a268d6e
Prashish GT <Prashishgtusers.name>
2023-04-14 13:51:00 +0545

    First commit

---
+++ client/src/state/index.js
@@ -0,0 +1,41 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    isCartOpen: false,
+    cart: [],
+    items: [],
+}
+
+export const cartSlice = createSlice({
+    name:'cart',
+    initialState,
+    reducers:{
+        setItems: (state, action) => {
+            state.items = action.payload;
+        },
+        addToCart: (state, action) => {
+            state.cart = [...state.cart, action.payload.item];
+        },
+        removeFromCart: (state, action) => {
+            state.cart = state.cart.filter((item) => item.id !== action.payload.id)
+        },
+        increaseCount: (state, action) => {
+            if (item.id === action.payload.id) {
+                item.count++;
+            } 
+            return item;
+        },
+        decreaseCount : (state, action) => {
+            if (item.id === action.payload.id && item.count > 1) {
+                item.count--
+            } 
+            return item;
+        }
+    }
+});
+
+export const {
+    setItems
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
